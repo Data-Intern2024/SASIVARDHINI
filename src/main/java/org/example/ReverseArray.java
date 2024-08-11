087172d7acd863271a3b7e4b24adb4e85ab96966
sasivardhini <sasivardhinisrinivasan@gmail.com>
2024-08-11 11:30:16 +0530

    reversearray changed

--- src/main/java/org/example/ReverseArray.java
+++ src/main/java/org/example/ReverseArray.java
@@ -3,8 +3,11 @@ package org.example;
 public class ReverseArray {
     public static void main(String[] args) {
         int array[]={12,13,1,1,1,21,1};
-        for(int i=0;i< array.length;i++){
-            array[i]=array[array.length-1];
+        int a= array.length;
+        for(int i=0;i< a/2;i++){
+            int temp=array[i];
+            array[i]=array[a-1-i];
+            array[a-1-i]=temp;
         }
         for (int i=0;i< array.length;i++){
             System.out.println(array[i]);
